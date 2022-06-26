b6bd40975baea46ac380120ba8b6909f6bef8aa0
Bryan Boot <bboot1@gmail.com>
2022-06-25 19:10:48 -0700

    no --release

--- generate.sh
+++ generate.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 PY_SRC="${SCRIPT_DIR}/src/bdkpython/"
 
 echo "Generating bdk.py..."
-BDKFFI_BINDGEN_OUTPUT_DIR="$PY_SRC" BDKFFI_BINDGEN_PYTHON_FIXUP_PATH=bdkffi cargo run --manifest-path ./bdk-ffi/Cargo.toml --package bdk-ffi-bindgen -- --language python --udl-file ./bdk-ffi/src/bdk.udl --release
+BDKFFI_BINDGEN_OUTPUT_DIR="$PY_SRC" BDKFFI_BINDGEN_PYTHON_FIXUP_PATH=bdkffi cargo run --manifest-path ./bdk-ffi/Cargo.toml --package bdk-ffi-bindgen -- --language python --udl-file ./bdk-ffi/src/bdk.udl
